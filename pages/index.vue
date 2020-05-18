4ad8c923449a3ad80bde8b700aee00a35118185e
izumi-love <izumi.love.kagoshima@gmail.com>
2020-05-18 15:55:21 +0900

    縦長(縦向き)時に縦書き、横長(横向き)時に横書きにした

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,46 @@
 <template>
   <div class="container">
-    <div>
-      <h1>
+    <h1 class="font-sans text-6xl font-light tracking-wider text-gray-600" v-bind:class="{ 'vertical-text': isPortrait }">
         イズミラヴ
-      </h1>
-    </div>
+    </h1>
   </div>
 </template>
 
-<style>
+<script>
+export default {
+  data: function () {
+    return {
+      isPortrait: true,   // 縦長(縦向き)
+      isLandscape: false, // 横長(横向き)
+    }
+  },
+  mounted () {
+    window.addEventListener('resize', this.resizeWindow, false);
+    window.addEventListener('orientationchange', this.resizeWindow, false);
+    this.resizeWindow();
+  },
+  methods: {
+    resizeWindow () {
+      let width = window.innerWidth;
+      let height = window.innerHeight;
+      this.isPortrait = (width <= height);
+      this.isLandscape = (width >= height);
+    },
+  },
+  beforeDestroy: function () {
+    window.removeEventListener('resize', this.resizeWindow, false);
+    window.removeEventListener('orientationchange', this.resizeWindow, false);
+  },
+}
+</script>
+
+<style scoped>
 .container {
   @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+
+.vertical-text {
+  writing-mode: vertical-rl;
+  text-orientation: upright;
 }
 </style>
