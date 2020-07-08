caa1db2b9c0076ef8b80029eedd4570e2327ee2e
izumi-love <izumi.love.kagoshima@gmail.com>
2020-07-08 16:57:09 +0900

    背景画像と枠線の追加とフォントを白く変更した

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,17 @@ module.exports = {
     fontSize: {
       '6xl': '4rem',
       '7xl': '5rem',
-    }
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+      '8': '8px',
+      '12': '12px',
+      '16': '16px',
+      '24': '24px',
+    },
   },
   variants: {},
   plugins: []
